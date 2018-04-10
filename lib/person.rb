743d9bfbf625c2889ed8453c9659a6bc29b176b0
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-10 17:54:15 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,3 @@
 class Person
- class Person
-irb(main):059:1>   def initialize(name)
-irb(main):060:2>     @name = name
-irb(main):061:2>   end
-irb(main):062:1>  
-irb(main):063:1*   def name=(name)
-irb(main):064:2>     @name = name
-irb(main):065:2>   end
-irb(main):066:1>  
-irb(main):067:1*   def name
-irb(main):068:2>     @name
-irb(main):069:2>   end
-irb(main):070:1> end
+
 end
